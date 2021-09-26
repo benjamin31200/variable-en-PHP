74d6d7f7a208cdcdbedc9e231b4e86e68ce9215d
Benjamin Deneux <benjamindeneux@hotmail.com>
2021-09-26 18:40:19 +0200

    les variables en PHP

---
+++ variable.php
@@ -0,0 +1,24 @@
+<?php
+
+function boléen(){
+    $text = "Have you seen Indiana Jones and the last crusade ?:";
+    $movieInfo = "$text". "true";
+    echo "$movieInfo";
+    
+} 
+boléen();
+
+$movieName = "indiana Jones and the last crusade";
+echo "$movieName";
+
+$movieWatchInfo = &$boléen;
+echo "$movieWatchInfo";
+
+$releaseDate = 1989;
+echo $releaseDate;
+
+$IBMRating = 8.2;
+echo $IBMRating;
+
+?>
+
